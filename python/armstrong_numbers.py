600430e3203e603f5bae86eb98824616c4287472
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-26 15:25:30 -0500

    intital py solution

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,16 @@
 # How can you make this more scalable and reusable later?
+from posixpath import split
+
+
 def find_armstrong_numbers(numbers):
+    arm_nums=[]
+    for x in numbers:
+        total=0
+        power= len(str(x))
+        digits= list(str(x))
+        for n in digits:
+            total+= int(n)**power
+        if total == x:
+            arm_nums.append(x)
+    return arm_nums
+
